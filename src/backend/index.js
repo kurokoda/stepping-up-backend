65d2e31dc65d99feff39705b061f7e749ef384de
Kurokoda <indigo.developer@gmail.com>
2018-02-26 23:19:50 -0500

    Adding mongo DB

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const path    = require('path');
+const express  = require('express');
+const path     = require('path');
+const mongoose = require('mongoose');
+const config   = require('./config');
+
+let database;
+
+// Mongoose DB ----------------------------------------------------------------------
+
+if (config.MONGO_URI) {
+  mongoose.connect(config.MONGO_URI);
+  const database = mongoose.connection;
+  database.on('error', console.error.bind(console, 'connection error:'));
+  database.once('open', function () {
+    console.log('database connected')
+  });
+}
+
+// Setup ----------------------------------------------------------------------
+
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../..', 'frontend/build')));
 console.log(__dirname);
+
+// Routing ----------------------------------------------------------------------
 
 app.get('/api/foo', (req, res) => {
   res.send('Foo has been sent')
@@ -13,6 +33,8 @@ app.get('/api/foo', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../..', 'frontend/build/index.html'));
 });
+
+// Init ----------------------------------------------------------------------
 
 const port = process.env.PORT || 5000;
 
